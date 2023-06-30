b54dc52bbff8b94341d619041773333e32b9642d
Tibbe Rosbergen <tibberosbergen@mbp-van-tibbe.home>
2023-06-30 22:00:21 +0200

    test422

---
+++ treemap.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objs as go
+
+df = pd.read_csv(r'blkjck.csv')
+df = df.iloc[:, 1:]
+
+
+def treemap():
+    count_df = df.groupby(['PlayerNo', 'winloss', 'plybustbeat']).size().reset_index(name='count')
+
+    fig = px.treemap(count_df, path=['PlayerNo', 'winloss', 'plybustbeat'], values='count', color='PlayerNo',
+                 hover_data={'count': True})
+
+    fig.update_layout(title='Figuur 7: Treemap het resultaat van de gespeelde handen per speler')
+    return fig.show()
